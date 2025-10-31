6976b169e953571d005355eb37d7d9dde9ff84c4
Zaid Garcia <viczaidr@gmail.com>
2025-10-31 16:27:07 -0600

    Change Create view

--- solicitudMovimientosPcs/obj/Release/net8.0/solicitudMovimientosPcs.AssemblyInfo.cs
+++ solicitudMovimientosPcs/obj/Release/net8.0/solicitudMovimientosPcs.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("solicitudMovimientosPcs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20bbfdf59848b05c34657fc6012634ac06391c26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a810b116c11a9142c616c970682866dfbb00946")]
 [assembly: System.Reflection.AssemblyProductAttribute("solicitudMovimientosPcs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("solicitudMovimientosPcs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
